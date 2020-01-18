4c04fa1d5cb2ad4f1de64a782c4a3f0d111fb493
Leo Voon <voonlihhaur@gmail.com>
2020-01-18 11:57:19 +0800

    Add copy button

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 
-// Pointer click animation 
-function clickEffect(e){
-      var d=document.createElement("div");
-      d.className="clickEffect";
-      d.style.top=e.clientY+"px";d.style.left=e.clientX+"px";
-      document.body.appendChild(d);
-      d.addEventListener('animationend',function(){d.parentElement.removeChild(d);}.bind(this));
-    }
-    document.addEventListener('click',clickEffect);
 
+// Copy Code
+document.addEventListener('DOMContentLoaded', function (event) {
+  codecopy('pre') // your code tag selector!
+})
